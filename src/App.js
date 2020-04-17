1412e7231c5d53932f9c683264eac91b797be632
condrenjustind <condrenjustin@github.com>
2020-04-17 16:48:03 -0400

    added increment button

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Button from '@material-ui/core/Button'
 
 function App() {
+  var value = 0;
+  
+  function incrementVariable() {
+    value += 1;
+    console.log(value);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Button variant="contained" color = "primary" onClick={incrementVariable}>
+          Increment Me!
+        </Button>
         <a
           className="App-link"
           href="https://reactjs.org"
