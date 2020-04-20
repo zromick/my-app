c6bea75dd88f0a1c97b37dea06bf56733cdcbc1c
condrenjustind <condrenjustin@github.com>
2020-04-20 17:26:16 -0400

    added functionality and changed design

--- src/App.js
+++ src/App.js
@@ -4,27 +4,33 @@ import './App.css';
 import Button from '@material-ui/core/Button'
 import { Toolbar, AppBar } from '@material-ui/core';
 
-function App() {
-  var value = 0;
+class App extends React.Component{
 
-  function incrementVariable() {
-    value += 1;
-    console.log(value);
-  }
+  constructor(props){
+  super(props)
+  this.state = {count: 0}
+}
 
+incrementVariable = () => {
+  this.setState({
+    count: this.state.count + 1
+  })
+}
+
+  render(){
   return (
     <AppBar position = "static">
       <Toolbar>
-      <div className="App">
-      <header className="App-header">
-        <Button variant="contained" color = "primary" onClick={incrementVariable}>
+        <Button variant="contained" color = "secondary" variant="outlined" size="small" onClick={this.incrementVariable} 
+        fullWidth={true} disabled={this.state.count == 10}>
           Increment Me!
+          <br></br>
+          {this.state.count}
         </Button>
-      </header>
-    </div>
       </Toolbar>
     </AppBar>
   );
+}
 }
 
 export default App;
