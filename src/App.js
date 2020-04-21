15672c4c2258ef18af9e323b1de168f2825e65c7
condrenjustind <condrenjustin@github.com>
2020-04-20 23:13:35 -0400

    added grid functionality

--- src/App.js
+++ src/App.js
@@ -1,33 +1,83 @@
 import React from 'react';
 import './App.css';
 import Button from '@material-ui/core/Button'
-import { Toolbar, AppBar, Snackbar, Alert } from '@material-ui/core';
+import { Toolbar, AppBar, Snackbar, Grid} from '@material-ui/core';
 
 class App extends React.Component{
 
   constructor(props){
   super(props)
-  this.state = {count: 0}
+  this.state = {countOne: 0, countTwo: 0, countThree: 0}
 }
 
-incrementVariable = () => {
+incrementVariableOne = () => {
   this.setState({
-    count: this.state.count + 1
+    countOne: this.state.countOne + 1
+  })
+}
+
+incrementVariableTwo = () => {
+  this.setState({
+    countTwo: this.state.countTwo + 1
+  })
+}
+
+incrementVariableThree = () => {
+  this.setState({
+    countThree: this.state.countThree + 1
   })
 }
 
   render(){
   return (
     <AppBar position = "static">
-      <Toolbar>
-        <Button color = "secondary" variant="outlined" size="small" onClick={this.incrementVariable} 
-        fullWidth={true} disabled={this.state.count === 10}>
-          Increment Me
-          <br></br>
-          {this.state.count}
-        </Button>
-        <Snackbar open={this.state.count === 10} message="You have reached ten clicks!"/>
-      </Toolbar>
+    <Toolbar>
+      <Grid
+        container
+        direction = "row"
+        justify="space-between"
+        alignItems="center">
+          <Grid item >
+            <Button 
+              color = "secondary" 
+              variant="outlined" 
+              size="small" 
+              onClick={this.incrementVariableOne} 
+              disabled={this.state.countOne === 10}>
+                  Increment Me
+                  <br></br>
+                  {this.state.countOne}
+            </Button>
+          </Grid>
+          <Grid item >
+            <Button 
+              color = "secondary" 
+              variant="outlined" 
+              size="small" 
+              onClick={this.incrementVariableTwo} 
+              disabled={this.state.countTwo === 10}>
+                  Increment Me
+                  <br></br>
+                  {this.state.countTwo}
+            </Button>
+          </Grid>
+          <Grid item >
+            <Button 
+              color = "secondary" 
+              variant="outlined" 
+              size="small" 
+              onClick={this.incrementVariableThree} 
+              disabled={this.state.countThree === 10}>
+                  Increment Me
+                  <br></br>
+                  {this.state.countThree}
+            </Button>
+          </Grid>
+      </Grid>
+      <Snackbar 
+        open={this.state.countOne === 10 && this.state.countTwo === 10 && this.state.countThree === 10} 
+        message="You have reached ten clicks on each button!"/>
+    </Toolbar>
     </AppBar>
   );
 }
